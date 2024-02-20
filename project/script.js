3d75b2bf77579905e6bcf15bc79abb7a81f7729b
ValeriyaNow <wal.now@yandex.ru>
2024-02-20 20:25:13 +0300

    2

--- project/script.js
+++ project/script.js
@@ -7,36 +7,67 @@ const app1 = new Vue({
             title: 'Let’s Get Solution For Building Construction Work',
             textBox: 'Kitchan Design',
             datePublic: '26 December,2022',
+            geeks: {
+                width: '622.62px',
+                height: '415.08px',
+                margin: '-61.5px -119.19px -64.31px -163.43px'
+            }
+
         }, {
             id: '2',
             img: './img/blog2.png',
             title: 'Low Cost Latest Invented Interior Designing Ideas.',
             textBox: 'Living Design',
             datePublic: '26 December,2022',
+            geeks: {
+                width: '580px',
+                height: '386px',
+                margin: '-48px -119px -48px -119px'
+            }
         }, {
             id: '3',
             img: './img/blog3.png',
             title: 'Best For Any Office & Business Interior Solution',
             textBox: 'Interior Design',
             datePublic: '26 December,2022',
+            geeks: {
+                width: '945px',
+                height: '630px',
+                margin: '-138px -281px -202px -324px'
+            }
         }, {
             id: '4',
             img: './img/blog4.png',
             title: 'Let’s Get Solution For Building Construction Work',
             textBox: 'Kitchan Design',
             datePublic: '26 December,2022',
+            geeks: {
+                width: '524px',
+                height: '786px',
+                margin: '-167px -59px -329px -125px'
+            }
         }, {
             id: '5',
             img: './img/blog5.png',
             title: 'Low Cost Latest Invented Interior Designing Ideas.',
             textBox: 'Living Design',
             datePublic: '26 December,2022',
+            geeks: {
+                width: '418px',
+                height: '627px',
+                margin: '-242px -39px -95px -39px'
+            }
         }, {
             id: '6',
             img: './img/blog6.png',
             title: 'Best For Any Office & Business Interior Solution',
             textBox: 'Interior Design',
             datePublic: '26 December,2022',
+            geeks: {
+                width: '616px',
+                height: '412px',
+                margin: '-82px -111px -40px -165px'
+            }
         }]
     }
 })
